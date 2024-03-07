b1ced7a38ec6ed2755fe0fdcda59f544dbb35059
oleg <58697249+zerex21@users.noreply.github.com>
2024-03-07 18:13:25 +0300

    feat: add some controllers and inmemmory base

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,16 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { UserController } from './user/user.controller';
+import { TrackController } from './track/track.controller';
+import { ArtistController } from './artist/artist.controller';
+import { AlbumController } from './album/album.controller';
+import { FavsController } from './favs/favs.controller';
+
 
 @Module({
   imports: [],
-  controllers: [AppController],
+  controllers: [AppController, UserController, TrackController, ArtistController, AlbumController, FavsController],
   providers: [AppService],
 })
 export class AppModule {}

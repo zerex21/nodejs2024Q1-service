d55e353e82e81f9a37ef0abf7c3be4d75a7bcc5f
oleg <58697249+zerex21@users.noreply.github.com>
2024-03-09 20:00:57 +0300

    feat: add time change and update

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -6,13 +6,14 @@ import { v4 as uuidv4 } from 'uuid';
 import { base as mainBase } from "../../base";
 
 
+
 interface IUser {
     id: string,
     login: string,
     password: string,
     version: number,
-    createdAt: number,
-    updatedAt: number
+    createdAt: string,
+    updatedAt: string
 }
 
 const users = mainBase.Users
@@ -20,7 +21,27 @@ const checkUUID = /^[0-9A-F]{8}-[0-9A-F]{4}-[4][0-9A-F]{3}-[89AB][0-9A-F]{3}-[0-
 
 @Injectable()
 export class UserService {
-    private products = []
+
+
+    getCurrentDate(){
+        const currentDate = new Date();
+        const day = currentDate.getDate();
+        const month = currentDate.getMonth() + 1; // Месяцы начинаются с 0, поэтому добавляем 1
+        const year = currentDate.getFullYear();
+        const hours = currentDate.getHours();
+        const minutes = currentDate.getMinutes();
+
+        // Форматирование чисел меньше 10 для добавления ведущего нуля
+        const formattedDay = day < 10 ? `0${day}` : `${day}`;
+        const formattedMonth = month < 10 ? `0${month}` : `${month}`;
+        const formattedHours = hours < 10 ? `0${hours}` : `${hours}`;
+        const formattedMinutes = minutes < 10 ? `0${minutes}` : `${minutes}`;
+
+        // Сборка строки с датой в нужном формате
+        const formattedDate = `${formattedDay}.${formattedMonth}.${year} ${formattedHours}:${formattedMinutes}`;
+
+        return `${formattedDate}`;
+      }
 
     getAllUsers() {
         return users
@@ -55,9 +76,9 @@ export class UserService {
         const user = ({
             ...CreateUserDto,
             id: uuidv4(),
-            version: 1,
-            createdAt: 0,
-            updatedAt: 1
+            version: 0,
+            createdAt: this.getCurrentDate(),
+            updatedAt: ''
 
         })
 
@@ -81,12 +102,12 @@ export class UserService {
             if (p?.id === id) {
                 if(p.password !== UpdatePasswordDto.newPassword && p.password === UpdatePasswordDto.oldPassword){
                     p.password = UpdatePasswordDto.newPassword
+                    p.version++
+                    p.updatedAt= this.getCurrentDate()
                     return true
                 }else{
                     throw new HttpException('Check correct your passwords', HttpStatus.FORBIDDEN);
                 }
-
-
             }
         })
 
